b597bd64611bf2b6b5745b54013d60f2f248dd69
Daniel <maxdhs@gmail.com>
2018-10-31 21:46:11 -0700

    will create 3x3 grid with Xs

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+const PLAYER_TOKEN = "X"
+const COMPUTER_TOKEN = "O"
+
+
+$(document).ready(function(){
+
+  const grid = [
+    [" "," "," "],
+    [" "," "," "],
+    [" "," "," "]
+  ];
+  function isGameOver(){
+    
+  }
+
+$(".col").click(function(){
+  $this = $(this);
+
+  $(this).html(PLAYER_TOKEN);
+  const i = $this.data("i");
+  const j = $this.data("j");
+  grid[i][j] = PLAYER_TOKEN;
+
+  isGameOver()
+  });
+});
